7074946ce5e2896e3e8098683ae7e2335a655832
alanssitis <alanchunggt@gmail.com>
2022-12-29 17:37:36 -0500

    minor changes

--- private_dot_config/nvim/lua/alanssitis/packer.lua
+++ private_dot_config/nvim/lua/alanssitis/packer.lua
@@ -47,4 +47,6 @@ return require('packer').startup(function(use)
 	  }
   }
 
+  use 'gpanders/editorconfig.nvim'
+
 end)
--- private_dot_config/nvim/plugin/packer_compiled.lua
+++ private_dot_config/nvim/plugin/packer_compiled.lua
@@ -110,6 +110,11 @@ _G.packer_plugins = {
     path = "/home/achungma/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["editorconfig.nvim"] = {
+    loaded = true,
+    path = "/home/achungma/.local/share/nvim/site/pack/packer/start/editorconfig.nvim",
+    url = "https://github.com/gpanders/editorconfig.nvim"
+  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/home/achungma/.local/share/nvim/site/pack/packer/start/friendly-snippets",
