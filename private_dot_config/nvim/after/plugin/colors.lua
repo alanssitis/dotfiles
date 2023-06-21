6809dbf99679577fd7894b34c973bf672a13c294
alanssitis <alanchunggt@gmail.com>
2023-06-20 21:26:52 -0700

    change to rose-pine

--- private_dot_config/nvim/after/plugin/colors.lua
+++ private_dot_config/nvim/after/plugin/colors.lua
@@ -1,7 +1,7 @@
-require("monokai-pro").setup({
+require("rose-pine").setup({
   transparent_background = false
 })
 
-vim.cmd.colorscheme("monokai-pro")
+vim.cmd.colorscheme("rose-pine")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
