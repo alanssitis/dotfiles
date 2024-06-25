050af0ce7d29a56630202000e93754612556c04a
Alan Chung Ma <alanchunggt@gmail.com>
2024-06-25 11:13:28 -0400

    Make nvim transparent

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -639,6 +639,9 @@ require('lazy').setup({
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
     init = function()
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
